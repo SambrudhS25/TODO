02d1ac715cdc48d4275839c3a4f90dfc6ec04889
SambrudhS25 <sambrudhs007@gmail.com>
2025-08-15 14:09:10 +0530

    updates backend URI

--- kanbanFrontEnd/src/api/axiosConfig.ts
+++ kanbanFrontEnd/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.https://todo-3ndw.onrender.com/.1:5000/api", // backend URL
+  baseURL: "https://todo-3ndw.onrender.com/api", 
 });
 
 api.interceptors.request.use((config) => {
